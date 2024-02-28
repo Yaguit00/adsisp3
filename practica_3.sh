10c98cb05f641eaf29943321e7aa3a9d9dfb3172
Yaguit00 <torresgarciayago@gmail.com>
2024-02-28 12:40:53 +0100

    añadido leer fichero linea por linea

--- practica_3.sh
+++ practica_3.sh
@@ -2,9 +2,17 @@
 #878417, Torres Garcia, Yago, T, 3, A
 #870959, Pueyo Soria, Nicolas, T, 3, A
 
-if [ $EUID -ne 0 ]; then
-    echo "Este script necesita privilegios de administracion"
-    return
+if [ $# -ne 2 ]; then
+    echo "Numero incorrecto de parametros"
 else
-    echo "ole, tienes permisos"
+
+    if [ $EUID -ne 0 ]; then
+        echo "Este script necesita privilegios de administracion"
+        return
+    else
+        echo "ole, tienes permisos"
+        echo $(while IFS= read -r line; do
+            echo "$line"
+            done < prueba.txt)
+    fi
 fi

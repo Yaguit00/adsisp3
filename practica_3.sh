dd517ff542b18b6c55e0ad58c25fcb303046f5ec
Yaguit00 <torresgarciayago@gmail.com>
2024-02-28 13:42:23 +0100

    crea usuario con uidmin 1815

--- practica_3.sh
+++ practica_3.sh
@@ -10,7 +10,6 @@ else
         echo "Este script necesita privilegios de administracion"
         return
     else
-        echo "ole, tienes permisos"
         if [ $1 = "-a" ]; then
 
             while IFS=, read -r username passwd nombrecompleto; do
@@ -21,14 +20,16 @@ else
                     if [ $username = "" -o passwd = "" -o nombrecompleto = "" ];then
                         echo "Campo invalido"
                     else
-                        useradd -c $nombrecompleto -p $passwd -m -K PASS_MAX_DAYS=30 $username
+                        useradd -c $nombrecompleto -p $passwd -m -K PASS_MAX_DAYS=30 -K UID_MIN=1815 $username
                         echo "$nombrecompleto ha sido creado"
                     fi
                 fi
             done < $2
         elif [ $1 = "-s" ]; then
             while IFS=, read -r deleteme inutil inutil1; do
-                echo "borra el usuario $deleteme"
+                if [ $exists -ne 0 ]; then
+                    echo "borra el usuario $deleteme"
+                fi
             done < $2
         else
             echo "ponme parametros validos o que chupacharcos" 1>&2
